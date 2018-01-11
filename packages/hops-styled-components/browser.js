9b9ed8d10daaec5c25cc2dd11140697ec95e55a9
ZauberNerd <zaubernerd@zaubernerd.de>
2018-01-11 08:57:53 +0100

    WIP: implement styled-components support for hops

---
+++ packages/hops-styled-components/browser.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { combineContexts, ReactContext } from 'hops-react';
+import { ThemeProvider } from 'styled-components';
+
+export class StyledComponentsContext {
+  constructor(options = {}) {
+    this.theme = options.theme || {};
+  }
+
+  enhanceElement(reactElement) {
+    return React.createElement(
+      ThemeProvider,
+      { theme: this.theme },
+      reactElement
+    );
+  }
+}
+
+export const contextDefinition = StyledComponentsContext;
+export const createContext = combineContexts(
+  ReactContext,
+  StyledComponentsContext
+);
