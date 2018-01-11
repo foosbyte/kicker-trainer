7f0d6b3b6ed369bbb4ab643ec140e142b3eb3ef4
ZauberNerd <zaubernerd@zaubernerd.de>
2018-01-11 08:56:26 +0100

    chore: remove styles

--- src/home/home.js
+++ src/home/home.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 
-import styles from './styles.css';
-
 export default function Home() {
   return (
     <div>
       <Helmet>
         <title>Hops Demo</title>
       </Helmet>
-      <h1 className={styles.headline}>Hello World!</h1>
+      <h1>Hello World!</h1>
     </div>
   );
 }
