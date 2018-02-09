75fb9abc292aa83c852d267ef91ecece76415d03
Markus Wolf <mail@markus-wolf.de>
2018-02-09 23:54:50 +0100

    refactor: add demo data

--- src/scenes/profile.tsx
+++ src/scenes/profile.tsx
@@ -1,7 +1,30 @@
 import * as React from 'react';
 
+import { Avatar } from '../components/avatar';
+import { Badge } from '../components/badge';
+import { Layout } from '../components/layout';
+
 export class Profile extends React.PureComponent {
   public render(): JSX.Element {
-    return <h1>profile</h1>;
+    return (
+      <div>
+        <h1>profile</h1>
+        <Avatar size="normal" />
+        <Layout type="column">
+          <Layout type="2-columns">
+            <Badge>Slice</Badge>
+            <div>38 mins</div>
+          </Layout>
+          <Layout type="2-columns">
+            <Badge>Brush</Badge>
+            <div>12 mins</div>
+          </Layout>
+          <Layout type="2-columns">
+            <Badge>Bande</Badge>
+            <div>32 mins</div>
+          </Layout>
+        </Layout>
+      </div>
+    );
   }
 }
