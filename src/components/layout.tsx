ee493d7ed8028823cd1773afddcf105ced6700d8
Markus Wolf <mail@markus-wolf.de>
2018-02-10 00:14:01 +0100

    feat: add content-bottom layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -17,6 +17,10 @@ function getTypeStyle(props: LayoutProps): any {
         flex-direction: column;
         align-items: center;
       `;
+    case 'content-bottom':
+      return css`
+        flex-direction: column;
+      `;
   }
 }
 
@@ -25,16 +29,36 @@ const LayoutWrapper = styled.div`
   ${(props: LayoutProps) => getTypeStyle(props)};
 `;
 
+const GrowingContent = styled.div`
+  flex-grow: 1;
+`;
+
 export interface LayoutProps {
-  type: 'centered-column' | '2-columns' | 'column' | 'row';
+  type: 'centered-column' | '2-columns' | 'column' | 'row' | 'content-bottom';
 }
 
 export class Layout extends React.PureComponent<LayoutProps> {
   public render(): JSX.Element {
     return (
       <LayoutWrapper type={this.props.type}>
-        {this.props.children}
+        {this.getLayoutMarkup()}
       </LayoutWrapper>
     );
+  }
+
+  private getLayoutMarkup(): React.ReactNode {
+    switch (this.props.type) {
+      case 'content-bottom': {
+        const children = React.Children.toArray(this.props.children);
+        return (
+          <>
+            <GrowingContent>{children[0]}</GrowingContent>
+            <div>{children[1]}</div>
+          </>
+        );
+      }
+      default:
+        return this.props.children;
+    }
   }
 }
--- src/scenes/profile.tsx
+++ src/scenes/profile.tsx
@@ -7,22 +7,25 @@ import { Layout } from '../components/layout';
 export class Profile extends React.PureComponent {
   public render(): JSX.Element {
     return (
-      <Layout type="centered-column">
-        <Avatar size="normal" />
-        <Layout type="column">
-          <Layout type="2-columns">
-            <Badge>Slice</Badge>
-            <div>38 mins</div>
-          </Layout>
-          <Layout type="2-columns">
-            <Badge>Brush</Badge>
-            <div>12 mins</div>
-          </Layout>
-          <Layout type="2-columns">
-            <Badge>Bande</Badge>
-            <div>32 mins</div>
+      <Layout type="content-bottom">
+        <Layout type="centered-column">
+          <Avatar size="normal" />
+          <Layout type="column">
+            <Layout type="2-columns">
+              <Badge>Slice</Badge>
+              <div>38 mins</div>
+            </Layout>
+            <Layout type="2-columns">
+              <Badge>Brush</Badge>
+              <div>12 mins</div>
+            </Layout>
+            <Layout type="2-columns">
+              <Badge>Bande</Badge>
+              <div>32 mins</div>
+            </Layout>
           </Layout>
         </Layout>
+        <div>bottom</div>
       </Layout>
     );
   }
