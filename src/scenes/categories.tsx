bce7ac47cdcf9526cac34f573e3d3ec2e32b8580
ZauberNerd <zaubernerd@zaubernerd.de>
2018-02-10 00:44:25 +0100

    refactor: update dummy scene data

--- src/scenes/categories.tsx
+++ src/scenes/categories.tsx
@@ -1,20 +1,16 @@
 import * as React from 'react';
 
 import { Layout } from '../components/layout';
-import { View } from '../components/view';
 
 export class Categories extends React.PureComponent {
   public render(): JSX.Element {
     return (
-      <View>
-        <h1>categories</h1>
-        <Layout type="equal-items" direction="column">
-          <div>Create workout</div>
-          <div>5 Bar Excercises</div>
-          <div>2 Bar Excercises</div>
-          <div>3 Bar Excercises</div>
-        </Layout>
-      </View>
+      <Layout type="equal-items" direction="column">
+        <div>Create workout</div>
+        <div>5 Bar Excercises</div>
+        <div>2 Bar Excercises</div>
+        <div>3 Bar Excercises</div>
+      </Layout>
     );
   }
 }
--- src/scenes/profile.tsx
+++ src/scenes/profile.tsx
@@ -7,23 +7,20 @@ import { Layout } from '../components/layout';
 export class Profile extends React.PureComponent {
   public render(): JSX.Element {
     return (
-      <Layout type="content-bottom" direction="column">
-        <Layout type="centered-column" direction="column">
-          <Avatar size="normal" />
-          <Layout type="items-left-right" direction="row">
-            <Badge>Slice</Badge>
-            <div>38 mins</div>
-          </Layout>
-          <Layout type="items-left-right" direction="row">
-            <Badge>Brush</Badge>
-            <div>12 mins</div>
-          </Layout>
-          <Layout type="items-left-right" direction="row">
-            <Badge>Bande</Badge>
-            <div>32 mins</div>
-          </Layout>
+      <Layout type="centered-column" direction="column">
+        <Avatar size="normal" />
+        <Layout type="items-left-right" direction="row">
+          <Badge>Slice</Badge>
+          <div>38 mins</div>
         </Layout>
-        <div>bottom</div>
+        <Layout type="items-left-right" direction="row">
+          <Badge>Brush</Badge>
+          <div>12 mins</div>
+        </Layout>
+        <Layout type="items-left-right" direction="row">
+          <Badge>Bande</Badge>
+          <div>32 mins</div>
+        </Layout>
       </Layout>
     );
   }
