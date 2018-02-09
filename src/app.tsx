8fd108c842e16f515ed539d512fc75e7b91025f1
ZauberNerd <zaubernerd@zaubernerd.de>
2018-02-10 00:48:37 +0100

    feat: add Layout and TabBar to App component

--- src/app.tsx
+++ src/app.tsx
@@ -1,39 +1,53 @@
-import * as React from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
-
 import { Miss, render } from 'hops-react';
 import { createContext } from 'hops-styled-components';
-import styled from 'styled-components';
+import * as React from 'react';
+import { Helmet } from 'react-helmet';
+import { Route, Switch } from 'react-router-dom';
+import styled, { injectGlobal } from 'styled-components';
+
+import { Layout } from './components/layout';
+import { TabBar, TabBarItem } from './components/tab-bar';
 
 import { Categories } from './scenes/categories';
 import { Exercise } from './scenes/exercise';
 import { Exercises } from './scenes/exercises';
 import { Profile } from './scenes/profile';
 
-const Root = styled.div`
-  text-align: center;
-  color: rebeccapurple;
+// tslint:disable-next-line:no-unused-expression
+injectGlobal`
+  body {
+    margin: 0;
+  }
+`;
+
+const Root = styled(Layout)`
+  height: 100vh;
 `;
 
 const App = (): JSX.Element => (
-  <Root>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <nav>
-      <Link to="/">Profile</Link>
-      <Link to="/categories">Categories</Link>
-    </nav>
-    <Switch>
-      <Route exact={true} path="/" component={Profile} />
-      <Route exact={true} path="/categories" component={Categories} />
-      <Route exact={true} path="/exercises/:category" component={Exercises} />
-      <Route
-        exact={true}
-        path="/exercises/:category/:name"
-        component={Exercise}
-      />
-      <Miss />
-    </Switch>
-  </Root>
+  <>
+    <Helmet>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Helmet>
+    <Root type="content-bottom" direction="column">
+      <Switch>
+        <Route exact={true} path="/" component={Profile} />
+        <Route exact={true} path="/categories" component={Categories} />
+        <Route exact={true} path="/exercises/:category" component={Exercises} />
+        <Route
+          exact={true}
+          path="/exercises/:category/:name"
+          component={Exercise}
+        />
+        <Miss />
+      </Switch>
+      <TabBar>
+        <TabBarItem title="Profile" to="/" />
+        <TabBarItem title="Categories" to="/categories" />
+        <TabBarItem title="Stats" to="/stats" />
+      </TabBar>
+    </Root>
+  </>
 );
 
 export default render(<App />, createContext());
