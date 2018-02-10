fd378a8eef24d5a9edec5b4678933f1176dc520d
ZauberNerd <zaubernerd@zaubernerd.de>
2018-02-10 03:34:14 +0100

    refactor: dummy implementation for exercises scene

--- src/scenes/exercises.tsx
+++ src/scenes/exercises.tsx
@@ -1,7 +1,83 @@
 import * as React from 'react';
+import styled from 'styled-components';
 
-export class Exercises extends React.PureComponent {
+import { Category } from '../components/category';
+import { Image } from '../components/image';
+import { ScrollContainer } from '../components/scroll-container';
+import { Text } from '../components/text';
+import { View } from '../components/view';
+
+const ExerciseWrapper = styled(View)`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const ExerciseImage = styled(View)`
+  width: 180px;
+`;
+
+interface ExerciseProps {
+  name: string;
+  cover: string;
+}
+
+class Exercise extends React.PureComponent<ExerciseProps> {
   public render(): JSX.Element {
-    return <h1>exercises</h1>;
+    return (
+      <ExerciseWrapper>
+        <Text>{this.props.name}</Text>
+        <ExerciseImage>
+          <Image source={this.props.cover} width={180} height={90} />
+        </ExerciseImage>
+      </ExerciseWrapper>
+    );
+  }
+}
+
+export interface ExercisesProps {
+  match?: {
+    params?: {
+      category?: string;
+    };
+  };
+}
+
+export class Exercises extends React.PureComponent<ExercisesProps> {
+  public render(): JSX.Element {
+    // const { match = {} } = this.props;
+    // const { params = {} } = match;
+    // const { category = '' } = params;
+
+    return (
+      <ScrollContainer>
+        <Category
+          title="5 Bar Excercises"
+          image={
+            <Image
+              source="https://dummyimage.com/320x148/000/fff"
+              width={320}
+              height={148}
+            />
+          }
+        />
+        <Exercise
+          name="Brush oben"
+          cover="https://dummyimage.com/180x90/000/fff"
+        />
+        <Exercise
+          name="Brush unten"
+          cover="https://dummyimage.com/180x90/000/fff"
+        />
+        <Exercise
+          name="Kantenpass"
+          cover="https://dummyimage.com/180x90/000/fff"
+        />
+        <Exercise
+          name="Kantenpass"
+          cover="https://dummyimage.com/180x90/000/fff"
+        />
+      </ScrollContainer>
+    );
   }
 }
