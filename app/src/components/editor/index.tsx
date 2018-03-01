5782debc9ca1bdaf75ef6bd8a9ca6e7243515078
Markus Wolf <mail@markus-wolf.de>
2018-03-02 00:10:22 +0100

    feat: draw rods on table

--- app/src/components/editor/index.tsx
+++ app/src/components/editor/index.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import styled from 'styled-components';
 
 import playfieldPath from './foosball-playfield.jpg';
+import rod2Path from './rod2.png';
 
 interface WrapperProps {
   width: number;
@@ -32,26 +33,49 @@ export interface EditorProps {
 export class Editor extends React.PureComponent<EditorProps> {
   private canvas?: HTMLCanvasElement | null;
   private playfield?: HTMLImageElement;
+  private rod2?: HTMLImageElement;
 
   public componentDidMount(): void {
-    this.playfield = new Image();
-    this.playfield.onload = () => {
+    this.playfield = this.loadImage(playfieldPath);
+    this.rod2 = this.loadImage(rod2Path);
+  }
+
+  private loadImage(path: string): HTMLImageElement {
+    const image = new Image();
+    image.onload = () => {
       this.forceUpdate();
     };
-    this.playfield.src = playfieldPath;
+    image.src = path;
+    return image;
   }
 
   public componentDidUpdate(): void {
-    if (this.canvas && this.playfield && this.playfield.src) {
+    if (this.canvas && this.playfield) {
       const ctx = this.canvas.getContext('2d');
       if (ctx) {
         ctx.drawImage(
-          this.playfield,
+          this.playfield!,
           0,
           0,
           this.props.width,
           this.props.height,
         );
+        // 1
+        ctx.drawImage(this.rod2!, -5, 35, 40, this.props.height);
+        // 2
+        ctx.drawImage(this.rod2!, 5, 0, 40, this.props.height);
+        // 3
+        ctx.drawImage(this.rod2!, 50, 0, 40, this.props.height);
+        // 5
+        ctx.drawImage(this.rod2!, 90, 0, 40, this.props.height);
+        // 5
+        ctx.drawImage(this.rod2!, 150, 0, 40, this.props.height);
+        // 3
+        ctx.drawImage(this.rod2!, 190, 0, 40, this.props.height);
+        // 2
+        ctx.drawImage(this.rod2!, 253, 0, 40, this.props.height);
+        // 1
+        ctx.drawImage(this.rod2!, 270, 35, 40, this.props.height);
       }
     }
   }
