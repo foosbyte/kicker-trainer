26d602ade59d2498b76bb4ada1af371613cce846
ZauberNerd <zaubernerd@zaubernerd.de>
2018-04-24 01:09:01 +0200

    feat: show all training progress on profile scene

--- app/src/stores/s3.ts
+++ app/src/stores/s3.ts
@@ -1,12 +1,22 @@
-export const Bars = {
-  '1bar': '1bar',
-  '2bar': '2bar',
-  '3bar': '3bar',
-  '5bar': '5bar',
-};
+export enum Bars {
+  '1bar' = '1bar',
+  '2bar' = '2bar',
+  '3bar' = '3bar',
+  '5bar' = '5bar',
+}
 
 export class S3 {
-  public data = {
+  public data: {
+    [B in Bars]: Array<{
+      bars?: {
+        blue: { [B1 in Bars]: number };
+        red: { [B1 in Bars]: number };
+      };
+      id: string;
+      name: string;
+    }>
+  } = {
+    [Bars['1bar']]: [],
     [Bars['2bar']]: [
       {
         bars: {
@@ -65,33 +75,47 @@ export class S3 {
     ],
   };
 
+  public getExercise(
+    id: string
+  ):
+    | {
+        id: string;
+        name: string;
+        bars?: {
+          blue: { [B1 in Bars]: number };
+          red: { [B1 in Bars]: number };
+        };
+      }
+    | undefined {
+    const exercises = Object.values(this.data).reduce(
+      (xs, x) => [...xs, ...x],
+      []
+    );
+    return exercises.find(e => e.id === id);
+  }
+
   public getBarPositions(
     id: string
   ): [
     { 1: number; 2: number; 3: number; 5: number },
     { 1: number; 2: number; 3: number; 5: number }
   ] {
-    const bar = Object.keys(this.data).find(b =>
-      this.data[b].some(e => e.id === id)
-    );
-    if (bar) {
-      const exercise = this.data[bar].find(e => e.id === id);
-      if (exercise && exercise.bars) {
-        return [
-          {
-            1: exercise.bars.blue['1bar'],
-            2: exercise.bars.blue['2bar'],
-            3: exercise.bars.blue['3bar'],
-            5: exercise.bars.blue['5bar'],
-          },
-          {
-            1: exercise.bars.red['1bar'],
-            2: exercise.bars.red['2bar'],
-            3: exercise.bars.red['3bar'],
-            5: exercise.bars.red['5bar'],
-          },
-        ];
-      }
+    const exercise = this.getExercise(id);
+    if (exercise && exercise.bars) {
+      return [
+        {
+          1: exercise.bars.blue['1bar'],
+          2: exercise.bars.blue['2bar'],
+          3: exercise.bars.blue['3bar'],
+          5: exercise.bars.blue['5bar'],
+        },
+        {
+          1: exercise.bars.red['1bar'],
+          2: exercise.bars.red['2bar'],
+          3: exercise.bars.red['3bar'],
+          5: exercise.bars.red['5bar'],
+        },
+      ];
     }
     return [{ 1: 0, 2: 0, 5: 0, 3: 0 }, { 1: 0, 2: 0, 5: 0, 3: 0 }];
   }
