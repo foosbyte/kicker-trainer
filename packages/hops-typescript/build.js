30bbe26544d4c05b747ecfd2ef2359aa4f0650f1
ZauberNerd <zaubernerd@zaubernerd.de>
2018-05-04 12:52:59 +0200

    chore: restrict function inlining to simple functions with uglify
    more information: https://github.com/mishoo/UglifyJS2/issues/2842

--- packages/hops-typescript/build.js
+++ packages/hops-typescript/build.js
@@ -1,5 +1,6 @@
 const baseHopsConfig = require('hops-build-config/configs/build');
 const typeScriptLoader = require('./typescript-loader');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 baseHopsConfig.resolve.extensions = typeScriptLoader.resolve.extensions.concat(
   baseHopsConfig.resolve.extensions
@@ -10,5 +11,25 @@ baseHopsConfig.resolve.mainFields = baseHopsConfig.resolve.mainFields.filter(
 baseHopsConfig.module.rules[0].oneOf.unshift(
   typeScriptLoader.module.rules[0].oneOf[0]
 );
+
+const pluginIndex = baseHopsConfig.optimization.minimizer.findIndex(
+  p => p instanceof UglifyJSPlugin
+);
+
+if (pluginIndex !== -1) {
+  baseHopsConfig.optimization.minimizer.splice(
+    pluginIndex,
+    1,
+    new UglifyJSPlugin({
+      cache: true,
+      parallel: true,
+      sourceMap: true,
+      uglifyOptions: {
+        compress: { inline: 1 },
+        output: { comments: false },
+      },
+    })
+  );
+}
 
 module.exports = baseHopsConfig;
