2fb109cbd97e316a88d5260be43968fbb8a4ade0
ZauberNerd <zaubernerd@zaubernerd.de>
2018-05-04 12:15:29 +0200

    feat: apply migrations after loading journal from storage

---
+++ app/src/stores/migrations/v1.ts
@@ -0,0 +1,13 @@
+import { Exercise } from '../training-journal';
+
+export function v1(exercises: Exercise[]): Exercise[] {
+  exercises.forEach(exercise => {
+    exercise.trainings.forEach(training => {
+      // we previously stored a Date instance, but now store miliseconds
+      training.date = new Date(training.date).getTime();
+      // quota wasn't present before, so we initialize it with zero
+      training.quota = Array.isArray(training.quota) ? training.quota : [0, 0];
+    });
+  });
+  return exercises;
+}
