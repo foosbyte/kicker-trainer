02747db978827c23b6a92161b54a3389635acd98
Markus Wolf <mail@markus-wolf.de>
2018-07-10 00:13:22 +0200

    feat: integrate a2hs prompt

--- app/src/stores/index.ts
+++ app/src/stores/index.ts
@@ -1,6 +1,7 @@
 import { Analytics } from './analytics';
 import { DataPrivacy } from './data-privacty';
 import { ExerciseCatalogue } from './exercise-catalogue';
+import { PWAIntegration } from './pwa';
 import { TrainingJournal } from './training-journal';
 import { TrainingSession } from './training-session';
 
@@ -9,3 +10,4 @@ export const trainingJournal = new TrainingJournal();
 export const trainingSession = new TrainingSession(trainingJournal);
 export const dataPrivacy = new DataPrivacy();
 export const analytics = new Analytics(dataPrivacy);
+export const pwa = new PWAIntegration(analytics);
