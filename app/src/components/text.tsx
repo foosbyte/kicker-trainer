102a9d8b18dbf3fc1e1dfedf54846040696fa9c3
ZauberNerd <zaubernerd@zaubernerd.de>
2019-01-03 14:22:41 +0100

    fix(Text): use correct type for base proptypes

--- app/src/components/text.tsx
+++ app/src/components/text.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 
-export class Text extends React.PureComponent<React.CSSProperties> {
+export type TextProps = React.HTMLAttributes<HTMLSpanElement> &
+  React.Props<HTMLSpanElement>;
+
+export class Text extends React.PureComponent<TextProps> {
   public render(): JSX.Element {
     const { children, ...props } = this.props;
     return <span {...props}>{children}</span>;
