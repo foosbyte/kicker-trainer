4d19b88b0999e8dea75aac09152a702b822a4d6c
ZauberNerd <zaubernerd@zaubernerd.de>
2019-02-23 17:26:13 +0100

    feat: allow colorized text
    Co-authored-by: Markus Wolf <mail@markus-wolf.de>

--- app/src/components/text.tsx
+++ app/src/components/text.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
+import { theme } from '../styled-components';
 
 export type TextProps = React.HTMLAttributes<HTMLSpanElement> &
-  React.Props<HTMLSpanElement>;
+  React.Props<HTMLSpanElement> & {
+    color?: keyof typeof theme['color'];
+  };
 
 export class Text extends React.PureComponent<TextProps> {
   public render(): JSX.Element {
-    const { children, ...props } = this.props;
-    return <span {...props}>{children}</span>;
+    const { children, color: themeColor, ...props } = this.props;
+    const color = themeColor ? theme.color[themeColor] : 'currentColor';
+    return (
+      <span {...props} style={{ color }}>
+        {children}
+      </span>
+    );
   }
 }
